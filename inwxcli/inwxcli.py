a804def4a272dce537f712a1d030239377ebfc49
Nico Bellack <nico@bellack.dev>
2025-01-21 12:48:15 +0100

    feat: set up Poetry and Nix flake

--- inwxcli/inwxcli.py
+++ inwxcli/inwxcli.py
@@ -3,34 +3,38 @@ import argparse
 from .inwx import domrobot, prettyprint
 
 
-class Domrobot():
+class Domrobot:
     def __init__(self, api_url, username, password):
         self.domrobot = domrobot(api_url, False)
-        self.domrobot.account.login({'lang': 'en', 'user': username, 'pass': password})
+        self.domrobot.account.login({"lang": "en", "user": username, "pass": password})
 
     def call(self, method_name, vars):
         method = self.domrobot
-        for method_name in method_name.split('.'):
+        for method_name in method_name.split("."):
             method = getattr(method, method_name)
         return method(vars)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--endpoint', help='API endpoint', default='https://api.domrobot.com/xmlrpc/')
-    parser.add_argument('--username', help='Username', required=True)
-    parser.add_argument('--password', help='Password', required=True)
+    parser.add_argument(
+        "--endpoint", help="API endpoint", default="https://api.domrobot.com/xmlrpc/"
+    )
+    parser.add_argument("--username", help="Username", required=True)
+    parser.add_argument("--password", help="Password", required=True)
     format_parser = parser.add_mutually_exclusive_group(required=False)
-    format_parser.add_argument('--format', help='Format')
-    format_parser.add_argument('--pretty', action='store_true', default=False, help='Attempt pretty print')
-    parser.add_argument('vars', nargs=argparse.REMAINDER)
+    format_parser.add_argument("--format", help="Format")
+    format_parser.add_argument(
+        "--pretty", action="store_true", default=False, help="Attempt pretty print"
+    )
+    parser.add_argument("vars", nargs=argparse.REMAINDER)
     return parser.parse_args()
 
 
 def parse_vars(vars):
     parsed_vars = {}
     for var in vars:
-        key, value = var.split('=', 1)
+        key, value = var.split("=", 1)
         parsed_vars[key] = value
     return parsed_vars
 
@@ -46,10 +50,10 @@ def main():
     if args.format:
         print(args.format.format(**result))
     elif args.pretty:
-        print(getattr(prettyprint, method_name.replace('.', '_'))(result))
+        print(getattr(prettyprint, method_name.replace(".", "_"))(result))
     else:
         print(result)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
